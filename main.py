e0148eaa5a0d0875a046ca99ac229e0da442e5c5
Eldiyar <edi.amanturov@gmail.com>
2023-01-15 08:37:07 +0600

    Json file operations completed

--- main.py
+++ main.py
@@ -1,52 +1,55 @@
+from typing import Union
+
 from fastapi import FastAPI
 import uvicorn
 
 import database
-
+import operations
 
 app = FastAPI()
 
-@app.get("/selectId")
-async def roo(id):
-    return database.start(id)
-
-@app.get("/")
-async def root():
-         return database.getAll()
-
-@app.delete("/delete")
-async def root(id):
-    return database.delete(id)
-
-
-@app.put("/update")
-async def root(id,isbn,name,author,section):
-    return database.update(id,isbn,name,author,section)
+@app.get("/read/All")
+async def readAll():
+    return operations.readAll()
+@app.get("/read")
+async def read(id):
+    return operations.read(id)
 
 @app.post("/create")
-async def root(id,isbn,name,author,section):
-    return database.create(id,isbn,name,author,section)
+async def create(name:str,price:float,description:Union[str,None]=None):
+
+    return operations.create(name,description,price)
+
+@app.put("/update")
+async def update(id,name:Union[str,None]=None,description:Union[str,None]=None,price:Union[float,None]=None):
+    return operations.update(id,name,description,price)
+
+@app.delete("/delete")
+async def delete(id):
+    return operations.delete(id)
 
 
 
-
-# @app.get("/read")
-# async def read():
-#     return operations.read()
+# @app.get("/selectId")
+# async def roo(id):
+#     return database.start(id)
 #
-# @app.get("/write")
-# async def write(id,name,description,price):
-#     return operations.write(id,name,description,price)
+# @app.get("/")
+# async def root():
+#          return database.getAll()
 #
-# @app.get("/update")
-# async def update(id,name,description,price):
-#     return operations.update(id,name,description,price)
+# @app.delete("/delete")
+# async def root(id):
+#     return database.delete(id)
 #
-# @app.get("/delete")
-# async def delete(id):
-#     return operations.delete(id)
 #
-
+# @app.put("/update")
+# async def root(id,isbn,name,author,section):
+#     return database.update(id,isbn,name,author,section)
+#
+# @app.post("/create")
+# async def root(id,isbn,name,author,section):
+#     return database.create(id,isbn,name,author,section)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1',port=8010)
--- operations.py
+++ operations.py
@@ -1,35 +1,65 @@
-# import json
-#
-#
-# def write(id,name,description,price):
-#
-#
-# def update(id,name,description,price):
-#     with open('text.json') as json_file:
-#         data = json.load(json_file)
-#         for p in data['Computers']:
-#             if p['id']== id:
-#                 p['name']=name
-#                 p['description'] = description
-#                 p['price'] = price
-#                 with open('text.json', 'w') as outfile:
-#                     json.dump(data, outfile)
-#
-# def read():
-#
-#     with open('text.json', 'r', encoding='utf-8') as f:
-#
-#         text = json.load(f)
-#         return text
-#
-# def delete(id):
-#     with open('text.json') as json_file:
-#         data = json.load(json_file)
-#         for p in data['Computers']:
-#             if p['id']== id:
-#                 p.pop('id'),
-#                 p.pop('name'),
-#                 p.pop('description'),
-#                 p.pop('price')
-#                 with open('text.json', 'w') as outfile:
-#                     json.dump(data, outfile)
+import json
+import random
+
+
+
+def readAll():
+    with open('Laptops.json') as f:
+        templates = json.load(f)
+
+    for section, commands in templates.items():
+        print(section)
+        print('\n'.join(map(str, commands)) + '\n')
+        return templates
+
+def read(id):
+    with open('Laptops.json') as f:
+        templates = json.load(f)
+
+    print(templates)
+
+    for section, commands in templates.items():
+        if (section == id):
+            return commands
+
+
+def create(name, description, price):
+    id = random.randint(0, 10000)
+
+    if (description == None):
+        description = 'No Description'
+
+    write = [id, name, description, price]
+    data = json.load(open("Laptops.json"))
+    data[id]=write
+    with open("Laptops.json", "w") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+def update(id, name, description, price):
+    with open('Laptops.json') as f:
+        file_content = f.read()
+        templates = json.loads(file_content)
+
+    for section, commands in templates.items():
+
+        if (section == id):
+            if name is not None:
+                commands[1] = name
+            if description is not None:
+                commands[2] = description
+            if price is not None:
+                commands[3] = price
+    to_json = templates
+
+    with open('Laptops.json', 'w') as f:
+        json.dump(to_json, f, sort_keys=True, indent=2)
+
+
+def delete(id):
+    with open('Laptops.json') as f:
+        file_content = f.read()
+        templates = json.loads(file_content)
+        templates.pop(id,None)
+    to_json = templates
+    with open('Laptops.json', 'w') as f:
+        json.dump(to_json, f, sort_keys=True, indent=2)
